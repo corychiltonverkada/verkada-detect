09f068816dc493623ae8ff0e6163a26e15781d00
Cory Chilton <cory.chilton@Cory-Chilton-FVFKK3AX1WG7.local>
2024-04-12 09:04:58 -0700

    comment clean-up

--- verkada_stream_utils.py
+++ verkada_stream_utils.py
@@ -4,12 +4,6 @@ import time
 import cv2
 from dotenv import load_dotenv, set_key
 
-# def get_streaming_token(api_key: str):
-#     token_url = "https://api.verkada.com/cameras/v1/footage/token"
-#     expiration = 3600
-#     headers = {"Content-Type": 'application/json', "x-api-key": api_key}
-#     response = requests.get(f"{token_url}?expiration={expiration}", headers=headers)
-#     return response.json()['jwt']
 STREAM_TIME = 3600  # seconds
 RESOLUTION = 'high_res'
 
